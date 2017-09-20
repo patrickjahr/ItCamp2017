24fdb67d2eb583f8d01d50243bbdc18ef220f79a
Patrick Jahr <patrick.jahr@googlemail.com>
2017-09-20 14:37:35 +0200

    add styling for list

--- src/app/list-item/list-item.component.ts
+++ src/app/list-item/list-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { StarWarsPerson } from 'models/starwarsPerson';
 
 @Component({
   selector: 'app-list-item',
@@ -7,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
 
-  @Input()name: string;
+  @Input()person: StarWarsPerson;
 
   constructor() { }
 
